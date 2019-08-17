1fbe774fcf09dfe56f9a8ca30989caab6fd74354
bunny67 <zolov6767@gmail.com>
2019-08-17 14:08:11 +0300

    update

--- ElvUI_SwingBar/Locales/English.lua
+++ ElvUI_SwingBar/Locales/English.lua
@@ -1,5 +1,5 @@
 ﻿-- English localization file for enGB.
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0-ElvUI")
 local L = AceLocale:NewLocale("ElvUI", "enUS", true)
 if not L then return end
 
--- ElvUI_SwingBar/Locales/Russian.lua
+++ ElvUI_SwingBar/Locales/Russian.lua
@@ -1,5 +1,5 @@
 ﻿--Файл локализации для ruRU
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0-ElvUI")
 local L = AceLocale:NewLocale("ElvUI", "ruRU")
 if not L then return end
 
